5de23d7adf2a28f3b3515eb084c57472d264015d
Christopher Kuttruff <kuttruff@gmail.com>
2013-11-05 12:41:57 -0800

    stress testing

--- sorting/main.c
+++ sorting/main.c
@@ -1,25 +1,29 @@
 // assert_values, inspect_arr
 #include "../helpers/testing.c"
-//#include "insert_sort.c"
+#include "insert_sort.c"
 #include "merge_sort.c"
 
+#define ARR_SIZE 1000000
 
 int main(void) {
   int i;
+  /*
   int test_set[][5] =  { {3, 1, 5, 2, 4},
 			 {5, 4, 3, 2, 1},
 			 {1, 2, 3, 4, 5},
 			 {2, 1, 3, 5, 4} };
   int expected[] = {1, 2, 3, 4, 5};
+  */
+  int arr[ARR_SIZE], expected[ARR_SIZE];
   int * res;
 
-  for(i = 0; i < 4; ++i) {
-    //res = insert_sort(test_set[i], 5);
-    res = merge_sort(test_set[i], 5);
-    inspect_array(test_set[i], 5);
-    inspect_array(res, 5);
-    assert_values(res, expected, 5);
-  }
+  for(i = 0; i < ARR_SIZE; ++i)
+    expected[i] = i;
+  for(i = ARR_SIZE - 1; i >= 0; --i)
+    arr[i] = i;
+
+  res = merge_sort(arr, ARR_SIZE);
+  assert_values(res, expected, ARR_SIZE);
 
   free(res);
 
